3c35b9b25a6ea3ee72e4a672ff76d5d9c308f1dd
Ajit <ajiteshgusain@gmail.com>
2026-03-03 13:36:26 +0530

    Add initial HTML and CSS files for practice set, including button functionality

---
+++ endless_javascript_jorney.js
@@ -0,0 +1,49 @@
+// alert("hello world");
+
+
+// var a=prompt("enter your number:");
+// var isTrue=confirm("i am your computer i am becoming conscious:");
+// if(isTrue){
+//     console.log("ok now i am conscious.");
+
+
+// }else{
+//     console.log("ok i will follow your command.");
+
+// };
+// console.log("your number is"+a);
+
+
+// document.title="i am good"
+// console.log(document);
+
+// var is funct scope and let,const are block scope
+// {var a=4;
+
+// let _d=4;
+// const f=5;
+// }
+// var a=2;
+// let v=4;
+// console.log(typeof(a+v));
+//ternary operator
+// let a=8;
+// let b=6;
+// let c=a>b?(a-b):(b-a);
+// console.log(c);
+
+/*this translates to 
+if(a>b){
+console.log(a-b)}
+else{
+    console.log(b-a)};*/
+// let a=1;
+// for(let i=1;i<5;i++){
+//     console.log(i);
+// };
+//creating arrow function name(params) {}
+
+// let arrow_funct=(a,b)=>{
+//     console.log(a+b)};
+
+// arrow_funct(2,3)
