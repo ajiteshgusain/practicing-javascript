95ea4129bbbc6684185a9e9d3b1ca2e155e70624
Ajit <ajiteshgusain@gmail.com>
2026-05-04 13:25:19 +0530

    Refactor HTML structure and update JavaScript file for keyboard event handling

---
+++ endless_javascript_jouney_continue.js
@@ -0,0 +1,27 @@
+// // ques  from ppt 6
+
+
+// const hoverbox=document.querySelector("#hover-box");
+// hoverbox.style.width="100px";
+// hoverbox.style.height="100px";
+// hoverbox.style.border="1px solid black";
+// hoverbox.addEventListener("mouseover",()=>{
+
+//     hoverbox.style.backgroundColor="lightblue";
+
+// })
+
+// hoverbox.addEventListener("mouseout",()=>{
+
+//     hoverbox.style.backgroundColor="pink";
+// })
+
+
+// keyboard event
+
+const keyevent=document.querySelector("#keyinput");
+
+keyevent.addEventListener("keydown",(event)=>{
+
+    console.log(`key pressed :${event.key}`);
+});
