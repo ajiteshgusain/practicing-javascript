07aece071f2a832d00257a6b5120fef817a5f0c8
Ajit <ajiteshgusain@gmail.com>
2026-05-27 09:37:38 +0530

    Add yamato_wave_motion_firing_sequence function with nested setTimeout calls

---
+++ practice set/js_pactice_for_endterm.js
@@ -0,0 +1,15 @@
+// callback function
+function yamato_wave_motion_firing_sequence(){
+    console.log("open the channel for wave motion energy.");
+
+
+    setTimeout(()=>{
+        console.log("tachyon partices rising in the chamber 120%");
+        setTimeout(()=>{
+            console.log("begin the firing the sequence ");
+            setTimeout(()=>{
+                console.log("wave motion gun fire.");
+            },1000)
+        },2000)
+    },3000)
+}
